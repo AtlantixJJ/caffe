6674aa8afad6f299c89d1f429987d49c638c4bb7
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-03-04 17:28:07 +0800

    g

--- examples/cifar_gan/run.sh
+++ examples/cifar_gan/run.sh
@@ -1,3 +1,5 @@
 mkdir log
-mkdir log/cifar_gan
-build/tools/caffe_gan train --g_solver=examples/cifar_gan/g_solver.prototxt --d_solver=examples/cifar_gan/d_solver.prototxt
+mkdir log/cifar_$1
+build/tools/caffe_gan train \
+    --g_solver=examples/cifar_gan/g_solver.prototxt \
+    --d_solver=examples/cifar_gan/d_solver.prototxt
