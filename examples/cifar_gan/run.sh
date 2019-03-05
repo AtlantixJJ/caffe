0bf727823fb4b813071af1c25b89a75b5088dce1
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-03-05 14:14:56 +0800

    d

--- examples/cifar_gan/run.sh
+++ examples/cifar_gan/run.sh
@@ -2,7 +2,7 @@ rm -r log/cifar_$1
 mkdir log
 mkdir log/cifar_$1
 echo net: \"examples/cifar_gan/$1_d.prototxt\" > examples/cifar_gan/d_solver.prototxt
-echo net: snapshot_prefix: \"log/cifar_pxs\" >> examples/cifar_gan/d_solver.prototxt
+echo snapshot_prefix: \"log/cifar_pxs\" >> examples/cifar_gan/d_solver.prototxt
 
 cat examples/cifar_gan/d_solver >> examples/cifar_gan/d_solver.prototxt
 
