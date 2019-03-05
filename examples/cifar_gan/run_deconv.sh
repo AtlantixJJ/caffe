d70ff8ad38b104447eb1cf37c93a9d0d0001b3f2
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-03-05 16:01:19 +0800

    d

---
+++ examples/cifar_gan/run_deconv.sh
@@ -0,0 +1,11 @@
+rm -r log/cifar_$1
+mkdir log
+mkdir log/cifar_$1
+echo net: \"examples/cifar_gan/$1_d.prototxt\" > examples/cifar_gan/d_solver.prototxt
+echo snapshot_prefix: \"log/cifar_$1\" >> examples/cifar_gan/d_solver.prototxt
+
+cat examples/cifar_gan/d_solver >> examples/cifar_gan/d_solver.prototxt
+
+build/tools/caffe_gan train \
+    --g_solver=examples/cifar_gan/deconv_g_solver.prototxt \
+    --d_solver=examples/cifar_gan/d_solver.prototxt
