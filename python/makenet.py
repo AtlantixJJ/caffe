d2ad7e5ef183da0ed4c1b8faf2972a3bd4b254b5
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-03-06 19:17:08 +0800

    c

--- python/makenet.py
+++ python/makenet.py
@@ -30,22 +30,22 @@ def create_cifar10_ae(input_file, batch_size=128):
     net.relu3 = L.ReLU(net.conv3, in_place=True, negative_slope=0.2)
     # 4x4
 
-    net.deconv3 = L.Deconvolution(net.relu2, convolution_param=dict(num_output=256, kernel_size=3, stride=2,
+    net.deconv3 = L.Deconvolution(net.relu3, convolution_param=dict(num_output=256, kernel_size=3, stride=2,
             pad=1, weight_filler=dict(type='gaussian', std=0.02)))
-    net.relu3 = L.ReLU(net.deconv3, in_place=True)
+    net.relu4 = L.ReLU(net.deconv3, in_place=True)
     # 8x8
 
-    net.deconv2 = L.Deconvolution(net.relu3, convolution_param=dict(num_output=256, kernel_size=3, stride=2,
+    net.deconv2 = L.Deconvolution(net.relu4, convolution_param=dict(num_output=256, kernel_size=3, stride=2,
             pad=1, weight_filler=dict(type='gaussian', std=0.02)))
-    net.relu2 = L.ReLU(net.deconv2, in_place=True)
+    net.relu5 = L.ReLU(net.deconv2, in_place=True)
     # 16x16
 
-    net.deconv1 = L.Deconvolution(net.relu2, convolution_param=dict(num_output=128, kernel_size=3, stride=2,
+    net.deconv1 = L.Deconvolution(net.relu5, convolution_param=dict(num_output=128, kernel_size=3, stride=2,
             pad=1, weight_filler=dict(type='gaussian', std=0.02)))
-    net.relu1 = L.ReLU(net.deconv2, in_place=True)
+    net.relu6 = L.ReLU(net.deconv1, in_place=True)
     # 32x32
 
-    net.conv_output = L.Convolution(net.relu1, num_output=64, kernel_size=3, stride=2,
+    net.conv_output = L.Convolution(net.relu6, num_output=64, kernel_size=3, stride=2,
             pad=1, weight_filler=dict(type='gaussian', std=0.02))
     net.output = L.TanH(net.conv_output)
 
