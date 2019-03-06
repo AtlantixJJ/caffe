6721942cbd83de53b3620aff862ff93e471d0f42
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-03-06 20:04:10 +0800

    stage

--- src/caffe/layers/l1loss_layer.cpp
+++ src/caffe/layers/l1loss_layer.cpp
@@ -1,94 +1,55 @@
-#ifndef CAFFE_L1_LOSS_LAYER_HPP_
-#define CAFFE_L1_LOSS_LAYER_HPP_
-
 #include <vector>
 
-#include "caffe/blob.hpp"
-#include "caffe/layer.hpp"
-#include "caffe/proto/caffe.pb.h"
-
-#include "caffe/layers/loss_layer.hpp"
+#include "caffe/layers/l1loss_layer.hpp"
+#include "caffe/util/math_functions.hpp"
 
 namespace caffe {
 
-/**
- * @brief Computes the L1 loss @f$
- *          E = \frac{1}{N} \sum\limits_{n=1}^N \left| \hat{y}_n - y_n
- *        \right| @f$ for real-valued regression tasks.
- *
- * @param bottom input Blob vector (length 2)
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the predictions @f$ \hat{y} \in [-\infty, +\infty]@f$
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the targets @f$ y \in [-\infty, +\infty]@f$
- * @param top output Blob vector (length 1)
- *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
- *      the computed L1 loss: @f$ E =
- *          \frac{1}{n} \sum\limits_{n=1}^N \left| \hat{y}_n - y_n
- *        \right| @f$
- *
- */
 template <typename Dtype>
-class L1LossLayer : public LossLayer<Dtype> {
- public:
-  explicit L1LossLayer(const LayerParameter& param)
-      : LossLayer<Dtype>(param), diff_() {}
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+void L1LossLayer<Dtype>::Reshape(
+  const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  LossLayer<Dtype>::Reshape(bottom, top);
+  CHECK_EQ(bottom[0]->count(1), bottom[1]->count(1))
+      << "Inputs must have the same dimension.";
+  diff_.ReshapeLike(*bottom[0]);
+}
 
-  virtual inline const char* type() const { return "L1Loss"; }
+template <typename Dtype>
+void L1LossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top) {
+  int count = bottom[0]->count();
+  caffe_sub(
+      count,
+      bottom[0]->cpu_data(),
+      bottom[1]->cpu_data(),
+      diff_.mutable_cpu_data());
+  Dtype loss = caffe_cpu_asum(count, diff_.cpu_data()) / bottom[0]->num();
+  top[0]->mutable_cpu_data()[0] = loss;
+}
 
-  virtual inline bool AllowForceBackward(const int bottom_index) const {
-    return true;
+template <typename Dtype>
+void L1LossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  for (int i = 0; i < 2; ++i) {
+    if (propagate_down[i]) {
+      const Dtype sign = (i == 0) ? 1 : -1;
+      const Dtype alpha = sign * top[0]->cpu_diff()[0] / bottom[i]->num();
+      caffe_cpu_sign(bottom[i]->count(),
+                     diff_.cpu_data(),
+                     bottom[i]->mutable_cpu_diff());
+      caffe_cpu_scale(bottom[i]->count(),
+                      alpha,
+                      bottom[i]->cpu_diff(),
+                      bottom[i]->mutable_cpu_diff());
+    }
   }
+}
 
- protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+#ifdef CPU_ONLY
+STUB_GPU(L1LossLayer);
+#endif
 
-  /**
-   * @brief Computes the L1 error gradient w.r.t. the inputs.
-   *
-   * Unlike other children of LossLayer, L1LossLayer \b can compute
-   * gradients with respect to the label inputs bottom[1] (but still only will
-   * if propagate_down[1] is set, due to being produced by learnable parameters
-   * or if force_backward is set). In fact, this layer is "commutative" -- the
-   * result is the same regardless of the order of the two bottoms.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
-   *      This Blob's diff will simply contain the loss_weight* @f$ \lambda @f$,
-   *      as @f$ \lambda @f$ is the coefficient of this layer's output
-   *      @f$\ell_i@f$ in the overall Net loss
-   *      @f$ E = \lambda_i \ell_i + \mbox{other loss terms}@f$; hence
-   *      @f$ \frac{\partial E}{\partial \ell_i} = \lambda_i @f$.
-   *      (*Assuming that this top Blob is not used as a bottom (input) by any
-   *      other layer of the Net.)
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the predictions @f$\hat{y}@f$; Backward fills their diff with
-   *      gradients @f$
-   *        \frac{\partial E}{\partial \hat{y}} =
-   *            \frac{1}{n} \sum\limits_{n=1}^N (\hat{y}_n - y_n)
-   *      @f$ if propagate_down[0]
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the targets @f$y@f$; Backward fills their diff with gradients
-   *      @f$ \frac{\partial E}{\partial y} =
-   *          \frac{1}{n} \sum\limits_{n=1}^N (y_n - \hat{y}_n)
-   *      @f$ if propagate_down[1]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-
-  Blob<Dtype> diff_;
-};
+INSTANTIATE_CLASS(L1LossLayer);
+REGISTER_LAYER_CLASS(L1Loss);
 
 }  // namespace caffe
-
-#endif  // CAFFE_L1_LOSS_LAYER_HPP_
