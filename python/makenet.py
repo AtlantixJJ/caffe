17db6863f6cc8d435a1d4cc2f6f128a639c7a662
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-03-06 19:13:01 +0800

    c

--- python/makenet.py
+++ python/makenet.py
@@ -9,7 +9,8 @@ cifar_lmda_dir = "examples/cifar10/cifar10_train_lmdb"
 def create_cifar10_ae(input_file, batch_size=128):
     net = caffe.NetSpec()
 
-    net.data = L.Data(batch_size=batch_size, source=cifar_lmda_dir, include=dict(phase=caffe.TRAIN))
+    net.data = L.Data(batch_size=batch_size, source=cifar_lmda_dir, include=dict(phase=caffe.TRAIN),
+        transform_param=dict(scale=1/128,mean_value=127.5))
 
     ## Convolutional Layer 1
     net.conv1 = L.Convolution(net.data, num_output=64, kernel_size=4, stride=2,
@@ -53,8 +54,7 @@ def create_cifar10_ae(input_file, batch_size=128):
 
     return net.to_proto()
 
-train_h5list_file = "a"
-output_file = "b"
+output_file = sys.argv[1]
 # batch_size = 50
 with open(output_file, 'w') as f:
-    f.write(str(create_cifar10_ae(train_h5list_file)))
+    f.write(str(create_cifar10_ae(cifar_lmda_dir)))
