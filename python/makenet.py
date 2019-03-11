9e520e8b818b3d257810471ce9cc9ec09b61119f
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-03-11 12:31:10 +0800

    d

--- python/makenet.py
+++ python/makenet.py
@@ -82,7 +82,7 @@ def create_cifar10_res_g(batch_size=128):
 def create_cifar10_res_d(batch_size=128):
     net = caffe.NetSpec()
 
-    net.data = L.Data(batch_size=batch_size, source=cifar_lmda_dir, include=dict(phase=caffe.TRAIN),
+    net.data, net.label = L.Data(batch_size=batch_size, source=cifar_lmda_dir, include=dict(phase=caffe.TRAIN),
         transform_param=dict(scale=1/128,mean_value=127.5))
 
     net.conv1 = L.Convolution(net.data, num_output=128, kernel_size=3, stride=1,
@@ -107,10 +107,10 @@ def create_cifar10_res_d(batch_size=128):
             pad=1, weight_filler=dict(type='gaussian', std=0.02))
     net.relu6 = L.ReLU(net.conv4, in_place=True) # 4x4
 
-    net.conv_output = L.Convolution(net.relu6, num_output=1, kernel_size=4, stride=1,
+    net.conv5 = L.Convolution(net.relu6, num_output=1, kernel_size=4, stride=1,
             pad=0, weight_filler=dict(type='gaussian', std=0.02))
-    net.output = L.TanH(net.conv_output)
-
+    
+    net.disc_loss = L.SigmoidCrossEntropyLoss(net.conv5, net.label)
     return net.to_proto()
 
 def create_cifar10_ae(batch_size=128):
