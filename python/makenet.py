77452d76295f937662f869ff29e40d5440a4b8f6
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-03-11 11:34:56 +0800

    d

--- python/makenet.py
+++ python/makenet.py
@@ -25,11 +25,11 @@ def simple_residual_block(name, net, x, dim, activation_fn, use_bn=True):
     if use_bn:
         bn2 = L.BatchNorm(conv2)
         add = L.Eltwise(x, bn2)
-        names = ['conv1', 'bn1', 'relu1', 'conv2', 'bn2', 'add']
+        names = ['conv1', 'bn1', 'act1', 'conv2', 'bn2', 'add']
         layers = [conv1, bn1, relu1, conv2, bn2, add]
     else:
         add = L.Eltwise(x, conv2)
-        names = ['conv1', 'relu1', 'conv2', 'add']
+        names = ['conv1', 'act1', 'conv2', 'add']
         layers = [conv1, relu1, conv2, add]
     
     for n,l in zip(names, layers):
@@ -48,30 +48,30 @@ def create_cifar10_res_g(batch_size=128):
     net.fc = L.InnerProduct(net.data, num_output=4*4*1024, weight_filler=dict(type='gaussian', std=0.02))
     net.reshape = L.Reshape(net.fc, reshape_param=dict(shape={'dim': [batch_size, 1024, 4, 4]}))
     net.bn = L.BatchNorm(net.reshape)
-    net.relu = L.RelU(net.bn, in_place=True)
+    net.relu = L.ReLU(net.bn, in_place=True)
 
     net.deconv1 = L.Deconvolution(net.relu, convolution_param=dict(num_output=512, kernel_size=3, stride=2,
             pad=1, weight_filler=dict(type='gaussian', std=0.02)))
     net.bn1 = L.BatchNorm(net.deconv1)
-    net.relu1 = L.RelU(net.bn1, in_place=True) # 8x8
+    net.relu1 = L.ReLU(net.bn1, in_place=True) # 8x8
 
     net.deconv2 = L.Deconvolution(net.relu1, convolution_param=dict(num_output=256, kernel_size=3, stride=2,
             pad=1, weight_filler=dict(type='gaussian', std=0.02)))
     net.bn2 = L.BatchNorm(net.deconv2)
-    net.relu2 = L.RelU(net.bn2, in_place=True) # 16x16
+    net.relu2 = L.ReLU(net.bn2, in_place=True) # 16x16
 
     net.res1_out = simple_residual_block("res1", net, net.relu2, 256, relu)
     net.bn3 = L.BatchNorm(net.res1_out)
-    net.relu3 = L.RelU(net.bn3, in_place=True)
+    net.relu3 = L.ReLU(net.bn3, in_place=True)
 
     net.res2_out = simple_residual_block("res2", net, net.relu3, 256, relu)
     net.bn4 = L.BatchNorm(net.res2_out)
-    net.relu4 = L.RelU(net.bn4, in_place=True)
+    net.relu4 = L.ReLU(net.bn4, in_place=True)
 
     net.deconv3 = L.Deconvolution(net.relu4, convolution_param=dict(num_output=128, kernel_size=3, stride=2,
             pad=1, weight_filler=dict(type='gaussian', std=0.02)))
     net.bn5 = L.BatchNorm(net.deconv3)
-    net.relu5 = L.RelU(net.bn5, in_place=True) # 32x32
+    net.relu5 = L.ReLU(net.bn5, in_place=True) # 32x32
 
     net.conv_output = L.Convolution(net.relu5, num_output=3, kernel_size=3, stride=1,
             pad=1, weight_filler=dict(type='gaussian', std=0.02))
@@ -87,25 +87,25 @@ def create_cifar10_res_d(batch_size=128):
 
     net.conv1 = L.Convolution(net.data, num_output=128, kernel_size=3, stride=1,
             pad=1, weight_filler=dict(type='gaussian', std=0.02))
-    net.relu1 = L.RelU(net.conv1, in_place=True, negative_slope=0.2) # 32x32
+    net.relu1 = L.ReLU(net.conv1, in_place=True, negative_slope=0.2) # 32x32
 
     net.conv2 = L.Convolution(net.relu1, num_output=256, kernel_size=4, stride=2,
             pad=1, weight_filler=dict(type='gaussian', std=0.02))
-    net.relu2 = L.RelU(net.conv2, in_place=True, negative_slope=0.2) # 16x16
+    net.relu2 = L.ReLU(net.conv2, in_place=True, negative_slope=0.2) # 16x16
 
     net.res1_out = simple_residual_block("res1", net, net.relu2, 256, lrelu, use_bn=False)
-    net.relu3 = L.RelU(net.res1_out, in_place=True, negative_slope=0.2)
+    net.relu3 = L.ReLU(net.res1_out, in_place=True, negative_slope=0.2)
 
     net.res2_out = simple_residual_block("res2", net, net.relu3, 256, lrelu, use_bn=False)
-    net.relu4 = L.RelU(net.res2_out, in_place=True)
+    net.relu4 = L.ReLU(net.res2_out, in_place=True)
 
     net.conv3 = L.Convolution(net.relu4, num_output=512, kernel_size=4, stride=2,
             pad=1, weight_filler=dict(type='gaussian', std=0.02))
-    net.relu5 = L.RelU(net.conv3, in_place=True) # 8x8
+    net.relu5 = L.ReLU(net.conv3, in_place=True) # 8x8
 
     net.conv4 = L.Convolution(net.relu5, num_output=1024, kernel_size=4, stride=2,
             pad=1, weight_filler=dict(type='gaussian', std=0.02))
-    net.relu6 = L.RelU(net.conv4, in_place=True) # 4x4
+    net.relu6 = L.ReLU(net.conv4, in_place=True) # 4x4
 
     net.conv_output = L.Convolution(net.relu6, num_output=1, kernel_size=4, stride=1,
             pad=0, weight_filler=dict(type='gaussian', std=0.02))
