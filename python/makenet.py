e995d3d6dd65e986e2c821fafe0a8e6b48d13150
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-03-11 10:13:43 +0800

    d

--- python/makenet.py
+++ python/makenet.py
@@ -6,7 +6,38 @@ from caffe.proto import caffe_pb2
 
 cifar_lmda_dir = "examples/cifar10/cifar10_train_lmdb"
 
-def create_cifar10_ae(input_file, batch_size=128):
+def relu(x):
+    return L.ReLU(x, in_place=True)
+
+def lrelu(x):
+    return L.ReLU(x, in_place=True, negative_slope=0.2)
+
+def simple_residual_block(name, net, x, dim, activation_fn):
+    conv1 = L.Convolution(x, num_output=dim, kernel_size=3, stride=1, pad=1,
+        weight_filler=dict(type="gaussian", std=0.02))
+    bn1 = L.BatchNorm(conv1)
+    relu1 = activation_fn(bn1)
+    conv2 = L.Convolution(relu1, num_output=dim, kernel_size=3, stride=1, pad=1,
+        weight_filler=dict(type="gaussian", std=0.02))
+    bn2 = L.BatchNorm(conv2)
+    add = L.Eltwise(x, bn2)
+    names = ['conv1', 'bn1', 'relu1', 'conv2', 'bn2', 'add']
+    layers = [conv1, bn1, relu1, conv2, bn2, add]
+    for n,l in zip(names, layers):
+        setattr(net, name + "_" + n, l)
+    return add
+
+def create_cifar10_res_g():
+    net = caffe.NetSpec()
+
+    net.data = L.Data(batch_size=batch_size, source=cifar_lmda_dir, include=dict(phase=caffe.TRAIN),
+        transform_param=dict(scale=1/128,mean_value=127.5))
+    
+    net.res1_out = simple_residual_block("res1", net, net.data, 256, relu)
+
+    return net.to_proto()
+
+def create_cifar10_ae(batch_size=128):
     net = caffe.NetSpec()
 
     net.data = L.Data(batch_size=batch_size, source=cifar_lmda_dir, include=dict(phase=caffe.TRAIN),
@@ -57,4 +88,4 @@ def create_cifar10_ae(input_file, batch_size=128):
 output_file = sys.argv[1]
 # batch_size = 50
 with open(output_file, 'w') as f:
-    f.write(str(create_cifar10_ae(cifar_lmda_dir)))
+    f.write(str(create_cifar10_res_g()))
