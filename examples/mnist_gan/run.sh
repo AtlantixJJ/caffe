6e2253495fa83ffe6c975a7d8a43d5f065f24576
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-03-12 22:14:19 +0800

    d

--- examples/mnist_gan/run.sh
+++ examples/mnist_gan/run.sh
@@ -1,3 +1,21 @@
+# ./run.sh <pool/pxs> <deconv/pxs>
+mkdir log
+rm -rf log/mnist_$1_$2
+mkdir log/mnist_$1_$2
+
+# make d solver
+cat d_solver > $1_$2_d_solver.prototxt
+echo "" >> $1_$2_d_solver.prototxt
+echo net: \"examples/mnist_gan/$1_d.prototxt\" >> $1_$2_d_solver.prototxt
+echo snapshot_prefix: \"log/mnist_$1_$2/\" >> $1_$2_d_solver.prototxt
+
+# make g solver
+cat g_solver > $1_$2_g_solver.prototxt
+echo "" >> $1_$2_g_solver.prototxt
+echo net: \"examples/mnist_gan/$2_g.prototxt\" >> $1_$2_g_solver.prototxt
+echo snapshot_prefix: \"log/mnist_$1_$2/\" >> $1_$2_g_solver.prototxt
+
 build/tools/caffe_gan train \
-    --d_solver=examples/mnist_gan/d_solver_mnist.prototxt \
-    --g_solver=examples/mnist_gan/g_solver_mnist.prototxt 2>&1 | tee log/mnist_pool/train.log
+    --d_solver=$1_$2_d_solver.prototxt \
+    --g_solver=$1_$2_g_solver.prototxt \
+    2>&1 | tee log/mnist_$1_$2/train.log
--- examples/mnist_gan/run_pxs.sh
+++
@@ -1,3 +0,0 @@
-build/tools/caffe_gan train \
-    --d_solver=examples/mnist_gan/d_pxs_solver_mnist.prototxt \
-    --g_solver=examples/mnist_gan/g_pxs_solver_mnist.prototxt 2>&1 | tee log/mnist_pxs/train.log
