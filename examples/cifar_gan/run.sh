e30d22b17d67d97df47fe688009640450b6f856f
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-03-12 22:22:08 +0800

    d

--- examples/cifar_gan/run.sh
+++ examples/cifar_gan/run.sh
@@ -1,15 +1,21 @@
-# ./run <d_proto> <g_proto>
-rm -r log/cifar_$1_$2
+# ./run.sh <pool/pxs> <deconv/pxs>
 mkdir log
+rm -rf log/cifar_$1_$2
 mkdir log/cifar_$1_$2
-# make d solver prototxt
-echo net: \"examples/cifar_gan/$1_d.prototxt\" > examples/cifar_gan/d_solver.prototxt
-echo snapshot_prefix: \"log/cifar_$1_$2\" >> examples/cifar_gan/d_solver.prototxt
-cat examples/cifar_gan/d_solver >> examples/cifar_gan/d_solver.prototxt
-# make g solver prototxt
-echo net: \"examples/cifar_gan/$1_g.prototxt\" > examples/cifar_gan/g_solver.prototxt
-cat examples/cifar_gan/g_solver >> examples/cifar_gan/g_solver.prototxt
+
+# make d solver
+cat examples/cifar_gan/d_solver > examples/cifar_gan/$1_$2_d_solver.prototxt
+echo "" >> examples/cifar_gan/$1_$2_d_solver.prototxt
+echo net: \"examples/cifar_gan/$1_d.prototxt\" >> examples/cifar_gan/$1_$2_d_solver.prototxt
+echo snapshot_prefix: \"log/cifar_$1_$2/\" >> examples/cifar_gan/$1_$2_d_solver.prototxt
+
+# make g solver
+cat examples/cifar_gan/g_solver > examples/cifar_gan/$1_$2_g_solver.prototxt
+echo "" >> examples/cifar_gan/$1_$2_g_solver.prototxt
+echo net: \"examples/cifar_gan/$2_g.prototxt\" >> examples/cifar_gan/$1_$2_g_solver.prototxt
+echo snapshot_prefix: \"log/cifar_$1_$2/\" >> examples/cifar_gan/$1_$2_g_solver.prototxt
 
 build/tools/caffe_gan train \
-    --g_solver=examples/cifar_gan/g_solver.prototxt \
-    --d_solver=examples/cifar_gan/d_solver.prototxt $3
+    --d_solver=examples/cifar_gan/$1_$2_d_solver.prototxt \
+    --g_solver=examples/cifar_gan/$1_$2_g_solver.prototxt \
+    2>&1 | tee log/cifar_$1_$2/train.log
