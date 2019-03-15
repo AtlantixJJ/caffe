c8c6dc6998c95b3f10b242ced973471e4ca8915a
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-03-15 15:32:44 +0800

    d

--- examples/vsp/run.sh
+++ examples/vsp/run.sh
@@ -1,21 +1,21 @@
-# ./run.sh <pool/pxs> <deconv/pxs>
+# ./run.sh <pool> <ae/unet>
 mkdir log
-rm -rf log/vsp_pool_$1
-mkdir log/vsp_pool_$1
+rm -rf log/vsp_$1_$2
+mkdir log/vsp_$1_$2
 
 # make d solver
-cat examples/vsp/d_solver > examples/vsp/pool_$1_d_solver.prototxt
-echo "" >> examples/vsp/pool_$1_d_solver.prototxt
-echo net: \"examples/vsp/pool_d.prototxt\" >> examples/vsp/pool_$1_d_solver.prototxt
-echo snapshot_prefix: \"log/vsp_pool_$1/\" >> examples/vsp/pool_$1_d_solver.prototxt
+cat examples/vsp/d_solver > examples/vsp/$1_$2_d_solver.prototxt
+echo "" >> examples/vsp/$1_$2_d_solver.prototxt
+echo net: \"examples/vsp/$1_d.prototxt\" >> examples/vsp/$1_$2_d_solver.prototxt
+echo snapshot_prefix: \"log/vsp_$1_$2/\" >> examples/vsp/$1_$2_d_solver.prototxt
 
 # make g solver
-cat examples/vsp/d_solver > examples/vsp/pool_$1_g_solver.prototxt
-echo "" >> examples/vsp/pool_$1_g_solver.prototxt
-echo net: \"examples/vsp/$1_g.prototxt\" >> examples/vsp/pool_$1_g_solver.prototxt
-echo snapshot_prefix: \"log/vsp_pool_$1/\" >> examples/vsp/pool_$1_g_solver.prototxt
+cat examples/vsp/d_solver > examples/vsp/$1_$2_g_solver.prototxt
+echo "" >> examples/vsp/$1_$2_g_solver.prototxt
+echo net: \"examples/vsp/$2_g.prototxt\" >> examples/vsp/$1_$2_g_solver.prototxt
+echo snapshot_prefix: \"log/vsp_$1_$2/\" >> examples/vsp/$1_$2_g_solver.prototxt
 
 build/tools/caffe_gan train \
-    --d_solver=examples/vsp/pool_$1_d_solver.prototxt \
-    --g_solver=examples/vsp/pool_$1_g_solver.prototxt \
-    2>&1 | tee log/vsp_pool_$1/train.log
+    --d_solver=examples/vsp/$1_$2_d_solver.prototxt \
+    --g_solver=examples/vsp/$1_$2_g_solver.prototxt \
+    2>&1 | tee log/vsp_$1_$2/train.log
