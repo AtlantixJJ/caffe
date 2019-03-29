debda5e05afacf2c6aa18e5c390182d6b73fa311
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-03-29 18:02:53 +0800

    d

--- runall.sh
+++ runall.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=2 bash examples/cifar_gan/run.sh pool deconv
-CUDA_VISIBLE_DEVICES=3 bash examples/cifar_gan/run.sh pool pxs
-CUDA_VISIBLE_DEVICES=4 bash examples/mnist_gan/run.sh pool deconv
-CUDA_VISIBLE_DEVICES=5 bash examples/mnist_gan/run.sh pool pxs
+CUDA_VISIBLE_DEVICES=2 bash examples/cifar_gan/run.sh pool deconv &
+CUDA_VISIBLE_DEVICES=3 bash examples/cifar_gan/run.sh pool pxs &
+CUDA_VISIBLE_DEVICES=4 bash examples/mnist_gan/run.sh pool deconv &
+CUDA_VISIBLE_DEVICES=5 bash examples/mnist_gan/run.sh pool pxs &
