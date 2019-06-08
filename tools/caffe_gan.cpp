d08da46ee6246a4f4773b11791be80774f017dce
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-06-08 16:54:56 +0800

    d

--- tools/caffe_gan.cpp
+++ tools/caffe_gan.cpp
@@ -320,7 +320,7 @@ int test() {
     float iter_loss;
     const vector<Blob<float>*>& result = caffe_net.Forward(&iter_loss);
     loss += iter_loss;
-    vector<cv::Mat> *imgs = caffe::blob2cv(result);
+    vector<cv::Mat> *imgs = caffe::blob2cv(&result);
     for (int j = 0; j < imgs->size(); j++)
       cv::imwrite(FLAGS_output + caffe::format_int(gind + i), (*imgs)[j]);
     gind += j;
