c3de9864e228198a89a9392af6d91ec6f98a26e4
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-06-09 01:21:34 +0800

    d

--- runall.sh
+++ runall.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES=2 bash examples/cifar_gan/run.sh pool deconv &
-CUDA_VISIBLE_DEVICES=3 bash examples/cifar_gan/run.sh pool pxs &
-CUDA_VISIBLE_DEVICES=4 bash examples/cifar_gan/run.sh pool upsample &
-CUDA_VISIBLE_DEVICES=2 bash examples/cifar_gan/run.sh res res &
-CUDA_VISIBLE_DEVICES=3 bash examples/mnist_gan/run.sh pool deconv &
-CUDA_VISIBLE_DEVICES=4 bash examples/mnist_gan/run.sh pool pxs &
-CUDA_VISIBLE_DEVICES=2 bash examples/mnist_gan/run.sh pool upsample &
+CUDA_VISIBLE_DEVICES=2 bash examples/cifar_gan/run_flags.sh pool deconv "--timing 1" &
+CUDA_VISIBLE_DEVICES=3 bash examples/cifar_gan/run_flags.sh pool pxs "--timing 1" &
+CUDA_VISIBLE_DEVICES=4 bash examples/cifar_gan/run_flags.sh pool upsample "--timing 1" &
+CUDA_VISIBLE_DEVICES=2 bash examples/cifar_gan/run_flags.sh res res "--timing 1" &
+CUDA_VISIBLE_DEVICES=3 bash examples/mnist_gan/run_flags.sh pool deconv "--timing 1" &
+CUDA_VISIBLE_DEVICES=4 bash examples/mnist_gan/run_flags.sh pool pxs "--timing 1" &
+CUDA_VISIBLE_DEVICES=2 bash examples/mnist_gan/run_flags.sh pool upsample "--timing 1" &
