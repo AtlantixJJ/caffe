53b63a08abaa9b89d627433d99aa06b6b87dff43
徐鉴劲 <xujj15@mails.tsinghua.edu.cn>
2019-06-08 21:58:11 +0800

    d

--- eval.sh
+++ eval.sh
@@ -1,13 +1,13 @@
 mkdir $4
 echo CUDA_VISIBLE_DEVICES=$1 build/tools/caffe_gan test \
     --gpu 0 \
-    --iterations 1000 \
+    --iterations 100 \
     --g_model $2 \
     --g_weights $3 \
     --output $4
 CUDA_VISIBLE_DEVICES=$1 build/tools/caffe_gan test \
     --gpu 0 \
-    --iterations 1000 \
+    --iterations 100 \
     --g_model $2 \
     --g_weights $3 \
     --output $4
